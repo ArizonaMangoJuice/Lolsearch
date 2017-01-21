627eca55aad3d6573b208addd806b459d18abbd3
Isael Lizama <Isaelwebdeveloper@gmail.com>
2017-01-20 19:53:09 -0800

    added the champion name

--- scripts/test.js
+++ scripts/test.js
@@ -29,7 +29,6 @@ $(document).ready(function() {
             dataType: "json",
             success: function(json) {
                 x = json.games.reverse();
-
                 for (var i = 0; i < x.length; i++) {
                     // this shows the kills 
                     var kills = x[i].stats.championsKilled;
@@ -49,9 +48,30 @@ $(document).ready(function() {
                     var monsters = x[i].stats.neutralMinionsKilled;
                     // this is the total cs
                     var cs = minions + monsters;
+                    //this is for the win or loss
                     var win = x[i].stats.win;
                     //this is the kda it still needs some work for kda's Showing NaN
                     var kda = ((x[i].stats.championsKilled + x[i].stats.assists) / x[i].stats.numDeaths).toFixed(2);
+                    //this is the champion id
+                    id = x[i].championId;
+                    var url3 = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + id + "?locale=en_US&champData=stats&api_key=RGAPI-c99b18d9-2a9d-43a4-8b94-29942275b32d"
+
+                    $.ajax({
+                        type: "GET",
+                        url: url3,
+                        async: false,
+                        dataType: "json",
+                        success: function(json) {
+                            var champion = json.id;
+                            if (champion == id) {
+                                championName = json.key;
+                            }
+                        },
+                        error: function() {
+                            alert("fucked somehting up");
+                        }
+                    });
+
                     if (win === true) {
                         win = "Victory";
                     } else {
@@ -85,12 +105,13 @@ $(document).ready(function() {
                     if (largestKill === undefined) {
                         largestKill = "0";
                     }
-                    console.log(win);
-                    console.log(x[i]);
+
+                    //console.log(x[i]);
+
                     //$(".info2").prepend("<li><a href= " + x[i].playerStatSummaryType + ">" + x[i].wins + "</a><p>" + x[i].playerStatSummaryType + "</p><li>")
                     $(".container-test").prepend("<div class='container-to-hold'><p class='border name-of-game'>" + x[i].subType + "</p><p class='stats'>" +
                         "" + kills + "/" + deaths + "/" + assists + "</p>" + "<p class='kda'>" + kda + " KDA" + "</p>" + "<p class='largest-kill'>" + "Killing Spree " + largestKill + "</p>" +
-                        "<p class='time'>" + time + " minutes" + "</p>" + "<p class='creep-score'>" + cs + " CS" + "</p>" + "<p class='outcome'>" + win + "</p>" + "</div>");
+                        "<p class='time'>" + time + " minutes" + "</p>" + "<p class='creep-score'>" + cs + " CS" + "</p>" + "<p class='outcome'>" + win + "</p>" + "<p class='champion-name'>" + championName + "</p>" + "</div>");
                     //$(".container-test").prepend("<p class='border'>" + x[i].playerStatSummaryType + "</p>");
                 }
 
@@ -103,3 +124,25 @@ $(document).ready(function() {
     });
 
 });
+
+
+/*
+
+                    if (id === champs) {
+
+                    }
+                    var championTest; 
+                            var url3 = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/+id+?locale=en_US&champData=stats&api_key=RGAPI-c99b18d9-2a9d-43a4-8b94-29942275b32d"
+        $.ajax({
+            type: "GET",
+            url: url3,
+            async: false,
+            dataType: "json",
+            success: function(json) {
+                if()
+            },
+            error: function() {
+                alert("fucked somehting up");
+            }
+        });
+*/
